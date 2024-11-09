fa9c36f55b79da5e9fca94e3ab923988ac061726
WangKang <888666wang4286@gmail.com>
2024-11-09 14:55:29 +0800

    提交dist

---
+++ dist/card.js
@@ -0,0 +1,142 @@
+export const cards = [
+  {
+    title: "zoterowin和Android软件下载",
+    details: "需要安装体验beta版本和zotero安卓版的朋友可以下载，安卓版会不定期开放体验槽",
+    image: "https://pic-go-42.oss-cn-guangzhou.aliyuncs.com/img/202410180846052.png",
+    link: "https://alist.wk8686.top/%E5%8D%9A%E5%AE%A2%E8%B5%84%E6%BA%90%E5%88%86%E4%BA%AB/01%20zotero%E8%B5%84%E6%BA%90%E5%90%88%E9%9B%86/02%20zotero_%E5%90%84%E7%89%88%E6%9C%AC%E5%AE%89%E8%A3%85%E5%8C%85",
+    title_link: "https://github.com/l0o0/translators_CN",
+    tags:["辅助阅读","辅助笔记","软件美化","其它内容"]
+  },
+  {
+    title: "搜索引擎合集",
+    details: "zotero的搜索引擎可以在多网页快速检索文献或条目，zotero7搜索引擎集成到侧栏体验更佳",
+    image: "https://pic-go-42.oss-cn-guangzhou.aliyuncs.com/img/202410172200842.png",
+    link: "http://wk8686.top/archives/zoteroepi69",
+    title_link: "https://github.com/l0o0/translators_CN",
+    tags:["其它内容","功能提升"]
+  },
+  {
+    title: "zotero-style",
+    details: "zotero的样式美化插件，包括自动期刊标签，笔记预览，注释自定义，阅读时间等",
+    image: "https://pic-go-42.oss-cn-guangzhou.aliyuncs.com/img/202410172200842.png",
+    link: "http://wk8686.top/archives/zoteroepi8",
+    title_link: "https://github.com/MuiseDestiny/zotero-style",
+    tags:["辅助阅读","软件美化"]
+  },
+  {
+    title: "zotero-addons",
+    details: "zotero的插件管理，其实zotero7正式版之后就不怎么需要了",
+    image: "https://pic-go-42.oss-cn-guangzhou.aliyuncs.com/img/202410172212541.svg",
+    link: "http://wk8686.top/archives/zoteroepi8",
+    title_link: "https://github.com/syt2/zotero-addons",
+    tags:["其它内容","功能提升"]
+    
+  },
+  {
+    title: "zotero-pdf-translate",
+    details: "必装插件，多语言翻译插件，支持谷歌翻译，百度翻译，deepl，谷歌翻译等",
+    image: "https://pic-go-42.oss-cn-guangzhou.aliyuncs.com/img/202410172214682.png",
+    link: "http://wk8686.top/archives/zoteroepi26",
+    title_link: "https://github.com/windingwind/zotero-pdf-translate",
+    tags:["辅助阅读","功能提升"]
+  },
+  {
+    title: "jasminum",
+    details: "必装插件，中文网页翻译器快速安装，包括知网",
+    image: "https://pic-go-42.oss-cn-guangzhou.aliyuncs.com/img/202410180842582.png",
+    link: "http://wk8686.top/archives/zoteroepi2",
+    title_link: "https://github.com/l0o0/jasminum",
+    tags:["其它内容"]
+  },
+  {
+    title: "zotero-reference",
+    details: "可以快速预览并导入当前论文的参考文献",
+    image: "https://pic-go-42.oss-cn-guangzhou.aliyuncs.com/img/202410180846052.png",
+    link: "http://wk8686.top/archives/zoteroepi28",
+    title_link: "https://github.com/MuiseDestiny/zotero-reference",
+    tags:["辅助阅读","其它内容"]
+  },
+  {
+    title: "zotero-citation",
+    details: "改进zotero的引用功能，实现拖拽引用和快捷键引用，写论文需要",
+    image: "https://pic-go-42.oss-cn-guangzhou.aliyuncs.com/img/202410180829489.png",
+    link: "http://wk8686.top/archives/zoteroepi32",
+    title_link: "https://github.com/MuiseDestiny/zotero-citation",
+    tags:["其它内容"]
+  },
+  {
+    title: "zotero-figure",
+    details: "侧栏展示当前论文的图片",
+    image: "https://pic-go-42.oss-cn-guangzhou.aliyuncs.com/img/202410180829659.png",
+    link: "http://wk8686.top/archives/zoteroepi47",
+    title_link: "https://github.com/MuiseDestiny/zotero-figure",
+    tags:["辅助阅读","软件美化"]
+  },
+  {
+    title: "tara",
+    details: "zotero备份插件，换机需要",
+    image: "https://pic-go-42.oss-cn-guangzhou.aliyuncs.com/img/202410180830314.png",
+    link: "http://wk8686.top/archives/zoteroepi52",
+    title_link: "https://github.com/l0o0/tara",
+    tags:["其它内容"]
+  },
+  {
+    title: "obsidian-zotlit",
+    details: "zotero-obsidian联动插件",
+    image: "https://pic-go-42.oss-cn-guangzhou.aliyuncs.com/img/202410180837325.png",
+    link: "http://wk8686.top/archives/zoteroepi88",
+    title_link: "https://github.com/PKM-er/obsidian-zotlit",
+    tags:["辅助笔记","其它内容"]
+  },
+  {
+    title: "Chartero",
+    details: "花里胡哨可以各种可视化zotero数据插件",
+    image: "https://pic-go-42.oss-cn-guangzhou.aliyuncs.com/img/202410180834925.svg",
+    link: "http://wk8686.top/archives/zoteroepi58",
+    title_link: "https://github.com/volatile-static/Chartero",
+    tags:["辅助阅读","软件美化"]
+  },
+  {
+    title: "zotero-format-metadata",
+    details: "可以规范zotero元数据插件，如日期作者，语言等，必装",
+    image: "https://pic-go-42.oss-cn-guangzhou.aliyuncs.com/img/202411072202353.webp",
+    link: "http://wk8686.top/archives/zoteroepi57",
+    title_link: "https://github.com/northword/zotero-format-metadata",
+    tags:["其它内容","辅助写作"]
+  },
+  {
+    title: "zotero-actions-tags",
+    details: "标签管理插件，同时可以自定义脚本执行指令",
+    image: "https://pic-go-42.oss-cn-guangzhou.aliyuncs.com/img/202410180831210.png",
+    link: "http://wk8686.top/archives/zoteroepi91",
+    title_link: "https://github.com/windingwind/zotero-actions-tags",
+    tags:["功能提升","其它内容"]
+  },
+  {
+    title: "zotero-better-notes",
+    details: "改进zotero官方的笔记功能，实现双链和笔记模板等",
+    image: "https://pic-go-42.oss-cn-guangzhou.aliyuncs.com/img/202410180836164.png",
+    link: "http://wk8686.top/archives/zoteroepi87",
+    title_link: "https://github.com/MuiseDestiny/zotero-better-notes",
+    tags:["辅助笔记","软件美化"]
+
+  },
+  {
+    title: "zotcard",
+    details: "卡片思维管理zotero笔记，喜欢零零散散笔记的人可以装",
+    image: "https://pic-go-42.oss-cn-guangzhou.aliyuncs.com/img/202410180829659.png",
+    link: "http://wk8686.top/archives/zoteroepi60",
+    title_link: "https://github.com/018/zotcard",
+    tags:["辅助笔记","软件美化"]
+  },
+  {
+    title: "zoplicate",
+    details: "zotero管理重复条目的插件，自动合并等",
+    image: "https://pic-go-42.oss-cn-guangzhou.aliyuncs.com/img/202410180832724.png",
+    link: "http://wk8686.top/archives/zoteroepi91",
+    title_link: "https://github.com/ChenglongMa/zoplicate",
+    tags:["功能提升","其它内容"]
+  }
+  
+  
+];
